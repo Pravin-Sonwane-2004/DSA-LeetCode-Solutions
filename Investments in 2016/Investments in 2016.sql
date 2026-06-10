cc73303e450c69353c7889428318283f99847ec4
pravin Sonwane <pravinson222@gmail.com>
2026-06-10 17:25:03 +0530

    ADD Investments in 2016

---
+++ Investments in 2016/Investments in 2016.sql
@@ -0,0 +1,22 @@
+-- # Write your MySQL query statement below
+-- select round(i.tiv_2016,2) as tiv_2016 
+-- from 
+-- Insurance i
+-- join Insurance j  
+-- on i.tiv_2015 = j.tiv_2016 
+
+
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+);
