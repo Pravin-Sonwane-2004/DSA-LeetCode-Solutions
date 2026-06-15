6449d1e98bcf0e069e0e912f023076c4321cbadb
pravin Sonwane <pravinson222@gmail.com>
2026-06-15 10:21:45 +0530

    ADD Delete the Middle Node of a Linked List

--- Delete the Middle Node of a Linked List/Delete the Middle Node of a Linked List.java
+++ Delete the Middle Node of a Linked List/Delete the Middle Node of a Linked List.java
@@ -10,6 +10,11 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+
         ListNode slow = head;
         ListNode fast = head;
         ListNode prev = null;
